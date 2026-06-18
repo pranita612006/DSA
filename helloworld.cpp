25421ed8a710bca79ebcb19bf6feb1a6d8b0e1e4
pranita612006 <ppawar612006@gmail.com>
2026-06-18 12:14:35 +0530

    hello world

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"hello wolrd"<<endl;
+	cout<<"please enter teh values of x and y";
+	int x,y;
+	cin>>x>>y;
+	cout<<"value of x is "<<x<<" and value of y is "<<y;
+	return 0;
+}
