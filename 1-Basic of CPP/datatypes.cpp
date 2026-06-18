e08695ef619929908d18a140af6d3cd609928fa3
pranita612006 <ppawar612006@gmail.com>
2026-06-18 14:50:12 +0530

    datatypes

---
+++ 1-Basic of CPP/datatypes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x = 10;
+    short s = 2;
+    float y = 3.05;
+    double d = 987652;
+    long l = 9888787;
+    char ch = 'A';
+    string str = "Hello";
+    cout<<"The integer value - "<<x<<endl<<"The float value - "<<y<<endl<<"The short value - "<<s<<endl<<"The double value - "<<d<<endl<<"The long value - "<<l<<endl<<"The char value - "<<ch<<endl<<"The string value - "<<str<<endl;
+return 0;
+}
