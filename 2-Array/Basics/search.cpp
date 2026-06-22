037d2e388168613cd2994f51b44261719871222f
pranita612006 <ppawar612006@gmail.com>
2026-06-22 07:33:48 +0530

    linear search

---
+++ 2-Array/Basics/search.cpp
@@ -0,0 +1,28 @@
+//WAF for linear search 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int target = 8;
+    int arra[4];
+    int count = 0;
+
+    cout<<"enter values: \n";
+    for(int i=0; i<4; i++){
+        cin>>arra[i];
+        if(arra[i]==target){
+            cout<<arra[i]<<" at "<<i+1<<endl;
+            count++;
+        }
+    }
+    if(count == 0){
+        cout<<"not found"<<endl;
+    }
+
+return 0;
+}
+
+/* Mistakes 
+1] i did not used count variable tried to get target using simple comparision 
+2] used = in if(arra[i]==target) 
+*/
