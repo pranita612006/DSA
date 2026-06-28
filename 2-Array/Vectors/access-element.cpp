dc98f42df62c360f02e598a5ffaa51f459c0bb96
pranita612006 <ppawar612006@gmail.com>
2026-06-28 19:22:26 +0530

    Access element and basic operations on vector

---
+++ 2-Array/Vectors/access-element.cpp
@@ -0,0 +1,27 @@
+//WAF to access elements from vector and perform different operations on vectors
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> vec = {1, 2, 3};
+    cout<<"Initial vector: ";
+    for (int i: vec){
+        cout<<i<<" ";
+    }
+    cout<<"\nSize of vector: "<<vec.size()<<endl;
+    vec.push_back(4);
+    cout<<"After push_back: ";
+    for (int i: vec){
+        cout<<i<<" ";
+    }
+    vec.pop_back();
+    cout<<"\nAfter pop_back: ";
+    for (int i: vec){
+        cout<<i<<" ";
+    }
+    cout<<"\nFront: "<<vec.front()<<" ";
+    cout<<"\nBack: "<<vec.back()<<" ";
+    cout<<"\nAccessed element: "<<vec.at(0);
+    return 0;
+}
